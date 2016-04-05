289729a7d7e99005675ccc92c5db7f6f769571f1
Diomidis Spinellis <dds@aueb.gr>
2016-04-05 16:25:48 +0300

    Relicense under Apache License

--- fileprune.c
+++ fileprune.c
@@ -1,19 +1,20 @@
-/*
+/*-
  *
- * fileprune - Prune a set of files, removing older copies
+ *  fileprune - Prune a set of files, removing older copies
  *
- * (C) Copyright 2002-2015 Diomidis Spinellis
+ *  Copyright 2002-2016 Diomidis Spinellis
  *
- * Permission to use, copy, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright notice appear in all copies and that
- * both that copyright notice and this permission notice appear in
- * supporting documentation.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 #include <sys/types.h>
